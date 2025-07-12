b4b9df171e60d93ec3eff681397d194ca01d0ee6
Gengo-bit <paulcorsino28@gmail.com>
2025-07-12 21:21:01 +0800

    add build command for vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // No rewrites needed since we're using Next.js API routes
+  output: 'standalone'
 }
 
 module.exports = nextConfig 
